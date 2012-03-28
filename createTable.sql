01b0c15922f378578f9f170e7100d1a2018fc796
hengyunabc1 <hengyunabc1@gmail.com>
2012-03-28 17:19:21 +0800

    add create table sql

---
+++ createTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `mytable` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `u` varchar(255) NOT NULL,
+  `pw` varchar(255) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
